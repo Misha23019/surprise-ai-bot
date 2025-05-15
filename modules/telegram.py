6693774f00d77455ce5d4d866a331afc506f4364
Misha23019 <misha.23019@gmail.com>
2025-05-15 14:36:25 +0300

    Update telegram.py

--- modules/telegram.py
+++ modules/telegram.py
@@ -1,17 +1,33 @@
 import requests
 
-def send_message(chat_id, text, token, keyboard=None):
+def send_message(chat_id, text, token, keyboard=None, inline_keyboard=None):
     url = f"https://api.telegram.org/bot{token}/sendMessage"
     payload = {
         "chat_id": chat_id,
-        "text": text
+        "text": text,
     }
+
     if keyboard:
         payload["reply_markup"] = {"keyboard": keyboard, "resize_keyboard": True}
+
+    if inline_keyboard:
+        payload["reply_markup"] = {
+            "inline_keyboard": inline_keyboard
+        }
+
     requests.post(url, json=payload)
 
 def build_keyboard(lang="uk"):
     return [
         [{"text": "🎲 Сюрприз"}, {"text": "🎥 Фільм"}],
-        [{"text": "🎧 Музика"}]
+        [{"text": "🎧 Музика"}, {"text": "📜 Цитата"}],
+        [{"text": "🔀 Рандом"}, {"text": "🍳 Рецепт"}]
+    ]
+
+def build_settings_inline():
+    return [
+        [
+            {"text": "🌐 Змінити мову", "callback_data": "change_language"},
+            {"text": "⏰ Змінити час", "callback_data": "change_time"}
+        ]
     ]

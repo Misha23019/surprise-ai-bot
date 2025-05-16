0876bdfaac87a52557a2db54b9d132fdaa747cfc
Misha23019 <misha.23019@gmail.com>
2025-05-16 13:14:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,2 @@
 #!/bin/bash
-
-# Активируем виртуальное окружение, если нужно
-# source venv/bin/activate
-
-export FLASK_APP=app.py
-export FLASK_ENV=production
-
-# Запускаем Flask с привязкой ко всем интерфейсам и порт из окружения
-flask run --host=0.0.0.0 --port=${PORT:-5000}
+python3 app.py

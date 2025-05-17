d9f82a7cc7854132f80d34f4961951979a793aca
Misha23019 <misha.23019@gmail.com>
2025-05-17 21:30:36 +0300

    Update bot.py

--- modules/bot.py
+++ modules/bot.py
@@ -3,10 +3,10 @@ import os
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
-TELEGRAM_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-if not TELEGRAM_API_TOKEN:
-    raise ValueError("❌ Переменная окружения TELEGRAM_API_TOKEN не установлена!")
+if not BOT_TOKEN:
+    raise ValueError("❌ Переменная окружения BOT_TOKEN не установлена!")
 
-bot = Bot(token=TELEGRAM_API_TOKEN)
+bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher(storage=MemoryStorage())

f2dfbf19ca90196b65c66d9bc8c6f7809aeb22af
Misha23019 <misha.23019@gmail.com>
2025-05-17 13:09:50 +0300

    Update bot.py

--- modules/bot.py
+++ modules/bot.py
@@ -1,6 +1,9 @@
 # modules/bot.py
 import os
-from aiogram import Bot, Dispatcher, types
+from aiogram import Bot, Dispatcher
+from aiogram.fsm.storage.memory import MemoryStorage
 
-bot = Bot(token=os.getenv("TELEGRAM_TOKEN"))
-dp = Dispatcher()
+TELEGRAM_API_TOKEN = os.getenv("TELEGRAM_API_TOKEN")
+
+bot = Bot(token=TELEGRAM_API_TOKEN)
+dp = Dispatcher(storage=MemoryStorage())

39edf5f7863a9899feab135e0dc78274c6b46eee
Misha23019 <misha.23019@gmail.com>
2025-05-17 18:05:12 +0300

    Update __init__.py

--- modules/__init__.py
+++ modules/__init__.py
@@ -1,10 +1,10 @@
 # modules/__init__.py
 
-# Экспортируем ключевые функции из подмодулей для удобного импорта
 from .lang import get_text, get_all_languages, get_flag
 from .limits import can_use, increase, reset_limits
 from .gpt_api import ask_gpt
-from .database import get_user, update_user, create_user
+from .database import get_user, create_user, update_user
 from .scheduler import schedule_daily_surprise
 from .telegram import send_surprise, build_keyboard
 from .router import handle_message
+from .texts import default_texts  # если есть словарь/контент

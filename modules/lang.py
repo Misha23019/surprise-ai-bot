913704a0405ca1503b30369759fd1fc842982685
Misha23019 <misha.23019@gmail.com>
2025-05-18 14:01:49 +0300

    Update lang.py

--- modules/lang.py
+++ modules/lang.py
@@ -3,7 +3,7 @@
 from aiogram import types
 from modules.database import update_user
 from modules.languages import LANGUAGES
-from modules.texts import TEXTS
+from modules.texts import default_texts
 
 async def ask_language(message: types.Message):
     keyboard = types.ReplyKeyboardMarkup(
@@ -18,7 +18,7 @@ async def ask_time(message: types.Message):
     await message.answer("Вкажіть свій місцевий час (наприклад, 15:30) – це потрібно, щоб надсилати сюрприз о 10:00 вашого часу.")
 
 def get_text(lang, key, default=None):
-    return TEXTS.get(lang, TEXTS["en"]).get(key, default if default is not None else key)
+    return default_texts.get(lang, default_texts["en"]).get(key, default if default is not None else key)
 
 async def save_language(user_id, lang_code):
     await update_user(user_id, {"lang": lang_code})

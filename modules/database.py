2c5e9c04e253b371c65ef7ed5f4c011e2fa5aec8
Misha23019 <misha.23019@gmail.com>
2025-05-20 08:07:01 +0300

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -1,39 +1,41 @@
 import json
 import os
 from datetime import datetime
+import aiofiles
 
 DB_FILE = "users.json"
 
-def load_db():
+async def load_db():
     if not os.path.exists(DB_FILE):
         return {}
-    with open(DB_FILE, "r") as f:
-        return json.load(f)
+    async with aiofiles.open(DB_FILE, "r") as f:
+        content = await f.read()
+        return json.loads(content)
 
-def save_db(data):
-    with open(DB_FILE, "w") as f:
-        json.dump(data, f)
+async def save_db(data):
+    async with aiofiles.open(DB_FILE, "w") as f:
+        await f.write(json.dumps(data, indent=2))
 
 async def get_user(user_id):
-    db = load_db()
+    db = await load_db()
     return db.get(str(user_id))
 
 async def save_user(user_id):
-    db = load_db()
+    db = await load_db()
     db[str(user_id)] = {
         "lang": "en",
         "limit": 5,
         "last_reset": str(datetime.utcnow().date()),
-        "time": "10:00"
+        "time": None  # пусть сначала сам введёт
     }
-    save_db(db)
+    await save_db(db)
 
 async def update_user(user_id, updates: dict):
-    db = load_db()
+    db = await load_db()
     user = db.get(str(user_id), {})
     user.update(updates)
     db[str(user_id)] = user
-    save_db(db)
+    await save_db(db)
 
 async def save_language(user_id, lang_code):
     await update_user(user_id, {"lang": lang_code})
